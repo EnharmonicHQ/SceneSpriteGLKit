2744b5d8fa9bd5f17947e689b5d1a9929cc2ac0e
Jonathan Saggau <jsaggau@enharmonichq.com>
2015-10-10 00:17:35 -0400

    Initial Commit

---
+++ SceneSpriteGLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SceneSpriteGLKit
+//
+//  Created by Jonathan Saggau on 10/10/15.
+//  Copyright © 2015 Enharmonic inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
